c840a4d66b04227757cb2111fb6b1ff26891de0b
Hans Adrian <hansccwolf@gmail.com>
2013-08-19 23:54:54 +0800

    Revert "Revert "Finding lectures by module code works""
    This reverts commit e6615c4b2fa6d5bf362fb6600b40aa8d843386e5.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+function BangBang() {
+    // on click:
+        // add image with class bang source /images/Pyang.png
+        var img = document.createElement("img");
+        img.src = "images/Pyang.png";
+
+        var class_name = "bang";
+        img.setAttribute("class", class_name);
+
+        OR
+
+        img.className = "bang";
+
+        // position its center at mouse position
+// var coordinates = [0,0];
+// d3.select('html') // Selects the 'html' element
+//   .on('mousemove', function()
+//     {
+//       coordinates = d3.mouse(this); // Gets the mouse coordinates with respect to
+//                                     // the top of the page (because I selected
+//                                     // 'html')
+//     });
+
+        // make random rotation
+        // fade out after 2 sec
+}
